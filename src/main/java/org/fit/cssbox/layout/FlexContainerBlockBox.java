5f32d8022ee1eb582f90dd712c87a14f215ea63c
Lukáš Ondrák <ondry.cz@seznam.cz>
2019-03-19 12:29:21 +0100

    Added major styles for flex container

--- src/main/java/org/fit/cssbox/layout/FlexContainerBlockBox.java
+++ src/main/java/org/fit/cssbox/layout/FlexContainerBlockBox.java
@@ -1,10 +1,24 @@
 package org.fit.cssbox.layout;
 
+import cz.vutbr.web.css.CSSProperty;
+import cz.vutbr.web.css.NodeData;
 import org.w3c.dom.Element;
 
 import java.awt.*;
 
 public class FlexContainerBlockBox extends BlockBox {
+
+    protected CSSProperty.FlexDirection flexDirection;
+    protected CSSProperty.FlexWrap flexWrap;
+    protected CSSProperty.JustifyContent justifyContent;
+    protected CSSProperty.AlignContent alignContent;
+    protected CSSProperty.AlignItems alignItems;
+
+    protected boolean isDirectionRow;
+    protected boolean isDirectionReversed;
+    protected int mainSpace;
+    protected int crossSpace;
+
 
     public FlexContainerBlockBox(Element n, Graphics2D g, VisualContext ctx) {
         super(n, g, ctx);
@@ -17,4 +31,46 @@ public class FlexContainerBlockBox extends BlockBox {
         typeoflayout = new FlexBoxLayoutManager(this);
         isblock = true;
     }
+
+    @Override
+    public void setStyle(NodeData s)
+    {
+        super.setStyle(s);
+        loadFlexContainerStyles();
+
+    }
+
+    public void loadFlexContainerStyles(){
+        flexDirection = style.getProperty("flex-direction");
+        if (flexDirection == null) flexDirection = CSSProperty.FlexDirection.ROW;
+        isDirectionRow = isDirectionRow();
+        isDirectionReversed = isDirectionReversed();
+
+        flexWrap = style.getProperty("flex-wrap");
+        if (flexWrap == null) flexWrap = CSSProperty.FlexWrap.NOWRAP;
+
+        justifyContent = style.getProperty("justify-content");
+        if (justifyContent == null) justifyContent = CSSProperty.JustifyContent.FlexStart;
+
+        alignContent = style.getProperty("align-content");
+        if (alignContent == null) alignContent= CSSProperty.AlignContent.Stretch;
+
+        alignItems = style.getProperty("align-items");
+        if (alignItems == null) alignItems= CSSProperty.AlignItems.Stretch;
+    }
+
+    public boolean isDirectionRow(){
+        if(flexDirection == CSSProperty.FlexDirection.ROW || flexDirection == CSSProperty.FlexDirection.ROW_REVERSE)
+            return true;
+        else
+            return false;
+    }
+
+    public boolean isDirectionReversed(){
+        if(flexDirection == CSSProperty.FlexDirection.COLUMN_REVERSE || flexDirection == CSSProperty.FlexDirection.ROW_REVERSE)
+            return true;
+        else
+            return false;
+    }
+
 }
